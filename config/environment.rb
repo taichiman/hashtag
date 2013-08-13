f0d2d7fa2a534a78a4c71ca03abb6286f6aef647
Denis Krivostchokov <densomart@gmail.com>
2013-08-13 22:30:34 +0700

    install rails 4

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+RailsHash::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsHash::Application.config.secret_key_base = 'b883ed6d1967ff57e58c0edaf08e73a0e8937a4ae70d2c5c78706db28487f10da361119e7701fe1a5d6152c3924e87e4b6e58105011f5af73066154906c9c9c1'
