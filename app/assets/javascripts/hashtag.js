dc28c738c281007d2abb5a9b2013b52841e74ce3
Denis Krivostchokov <densomart@gmail.com>
2013-08-19 19:13:08 +0700

    commmit

--- app/assets/javascripts/hashtag.js
+++ app/assets/javascripts/hashtag.js
@@ -1,18 +1,57 @@
+  start();
+
+  function start() {
+    VK.init(
+      function() { 
+        display_name();
+        create_hashtag_top();    
+      }, 
+      function() {},
+      '5.0'
+    )
+  }
+
+  function display_name() {
+    VK.api(
+      "users.get", 
+      {fields:"first_name, last_name", test_mode:"1"}, 
+      function(data) { 
+        var first_name = document.getElementById('response');
+        first_name.textContent = "Привет, "+data.response[0].first_name + ', '+ data.response[0].last_name;
+      }
+    )
+  }  
+
   function create_hashtag_top() {
     console.log('hello from func');
     var feed = get_user_feed();
-    var top_hashtag = parse( feed );
-    show( top_hashtag );
+    // var top_hashtag = parse( feed );
+    // show( top_hashtag );
+    return "Hashtags has shown";
   }
 
   function get_user_feed() {
-    return 'Feed';
+    VK.api(
+      "newsfeed.get", 
+      { start_time: 1376870400 }, 
+      function(data) { 
+        console.log( 'start get feed' );
+        if (data.response) { 
+         // data.response is object 
+         console.debug( data.response );         
+        } 
+        else {
+         console.error( data.error );         
+        }
+      }
+    )
+    return [];
   }
 
-  function parse( feed ){
-    return 'Ary hashtags';
-  }
+  // function parse( feed ){
+  //   return 'Ary hashtags';
+  // }
 
-  function show( hashtags ) {
-    $('#out').text( hashtag );
-  }
+  // function show( hashtags ) {
+  //   $('#out').text( hashtags );
+  // }
--- spec/javascripts/hashtagSpec.js
+++ spec/javascripts/hashtagSpec.js
@@ -1,5 +1,15 @@
-describe("Hashtag", function() {
-  it("get Feed", function() {
-    expect(get_user_feed()).toBe('Feed');
+  it("main function", function() {
+    expect( create_hashtag_top() ).toBe('Hashtags has shown');
   });
+
+describe("Feed getting", function() { 
+  it("get_user_feed", function() {
+    expect( Array.isArray( get_user_feed() ) ).toBe( true );
+  });
+  
+  it("get_user_feed", function() {
+    expect( Array.isArray( get_user_feed() ) ).toBe( true );
+  });
+  
 });
+
