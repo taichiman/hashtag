4e4727af145ef2b13a130ad195a9113162b4098d
Denis Krivostchokov <densomart@gmail.com>
2013-08-21 19:57:18 +0700

    Prepare for parsing:  Select the nessesary VK response fields

--- app/assets/javascripts/hashtag.js
+++ app/assets/javascripts/hashtag.js
@@ -34,29 +34,30 @@
 
     var count = 0;
     var counterPartsFeed = 1;
+    var filters = 'post, photo, photo_tag, note'
 
     VK.api(
       "newsfeed.get", 
-      { count: 100, start_time: 1376837770 }, 
+      { filters: filters, max_photos: 1, count: 100, start_time: 1376837769, end_time: 1377000273 }, 
       get_part_feed
     );
 
     function get_part_feed( data ) {
-      console.log( ' discover part feed :' + counterPartsFeed );
+      // console.log( ' discover part feed :' + counterPartsFeed );
       if (data.response) { 
         if ( data.response.items.length>0 ) {
-          console.debug( data.response );
+          // console.debug( data.response );
           counterPartsFeed++;
           allFeedItems = allFeedItems.concat( data.response.items )
-          console.info( allFeedItems );
+          // console.info( allFeedItems );
           VK.api(
           "newsfeed.get", 
-          { count: 100, offset: data.response.new_offset, start_time: 1376837770 }, 
+          { filters: filters, max_photos: 1, count: 100, offset: data.response.new_offset, start_time: 1376837769, end_time: 1377000273 }, 
           get_part_feed
           );
         }
         else{
-          console.log( 'End requests.' );
+          // console.log( 'End requests.' );
           parseFeed( allFeedItems );
         }
       } 
@@ -69,16 +70,30 @@
   }
 
   function parseFeed( feed ){
-    console.log( 'In parse.' )
-    console.log( allFeedItems )
-    z=document.createElement('textarea')
-    z.rows = 10;
-    z.cols = 40;
-    z.textContent = JSON.stringify( allFeedItems );
-    k=document.getElementsByTagName('p')[0];
-    k.appendChild( z );
-    console.log( k, z );
-    return 'Ary hashtags';
+    console.debug( 'In parse.' )
+    // var toClass = {}.toString;
+    // console.debug( toClass.call( feed[0] ) );
+    // console.debug( feed[0].toString() );
+    // console.debug( feed[0][1] instanceof Object );
+    // feed.forEach( function(x){ console.debug( x ) } );
+    
+    console.debug( feed[0] );
+    console.info( 'Follow lenght:' )
+
+    // for( i=0; i<feed[0].length; i++ ){
+    //   console.debug( feed[0][i] );
+    // }
+    for( p in feed[0] ){
+      console.debug( p );
+    }
+    
+    for( x=0; x<100; x++) { 
+      console.debug( feed[x].attachments )  
+    };
+    
+    // console.debug( feed );
+    // console.debug( feed[1] );
+    return feed;
   }
 
   // function show( hashtags ) {
